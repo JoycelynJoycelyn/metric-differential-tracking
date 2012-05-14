94256ffdc768acb01678b1f1e451f9066fcc8552
Tommaso Garuglieri <tommasogaruglieri@gmail.com>
2012-05-14 18:31:42 +0200

    cambiata metrica per il calcolo di G. usata la metrica Jaccard

--- G_vect.m
+++ G_vect.m
@@ -2,7 +2,7 @@ function g = G_vect(  A, pos_sample, neg_sample)
     sample = [pos_sample neg_sample];
   %  size(sample)
   %  pause()
-    X = exp(squareform( - (  pdist((A*sample)') .^2) ));
+    X = exp(squareform( - (  pdist((A*sample)','jaccard') .^2) ));
   %  size(X)
     %reshape(X, size(sample,2), size(sample,2)-1);
   %  size(X)
@@ -12,5 +12,8 @@ function g = G_vect(  A, pos_sample, neg_sample)
     pos_pij = X(1:size(pos_sample,2),1:size(pos_sample,2));
     neg_pij = X(size(pos_sample,2)+1:size(X,1),size(pos_sample,2)+1:size(X,1));
     g = sum(log(sum(pos_pij,2)-diag(pos_pij))) + sum(log(sum(neg_pij,2)-diag(neg_pij)));
+    if(isnan(g))
+        display('NaN');
+    end
 end
 
