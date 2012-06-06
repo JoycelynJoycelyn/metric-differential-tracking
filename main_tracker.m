bb89bca5467f7ea5e6f468ce90804d20ff728afd
Tommaso Garuglieri <tommasogaruglieri@gmail.com>
2012-06-06 14:49:07 +0200

    aggiornato track

--- main_tracker.m
+++ main_tracker.m
@@ -3,24 +3,24 @@ function T = main_tracker(fname)%, gamma, tau, radius)
 T.soglia_pos=0.7;
 T.soglia_neg=0.3;
 % numero di campioni per l'apprendimento
-T.num_sample_positivi = 15;
-T.num_sample_negativi = 40;
+T.num_sample_positivi = 30;
+T.num_sample_negativi = 100;
 % numero nuovi campioni a ogni frame
 T.campioni_pos_track = 5;
-T.campioni_neg_track = 10;
+T.campioni_neg_track = 5;
 % numero totale campioni per controllo di G(A)
-T.tot_campioni_pos = 10;
-T.tot_campioni_neg = 25;
+T.tot_campioni_pos = 5;
+T.tot_campioni_neg = 5;
 %soglia per G(A)
 T.threshold = 1.0e-2;
 %inizializzazione BB
 
 %box lemming
-%T.target.BB_q = [42  203   60   95];
+T.target.BB_q = [42  203   60   95];
 
 %T.target.BB_q = [86  319  146.0000   46.0000];
 %T.target.BB_q = [127  214   61   61];
-T.target.BB_q = [];
+%T.target.BB_q = [];
 
 
 
